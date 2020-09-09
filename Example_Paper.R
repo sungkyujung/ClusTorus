acab4d7f8a036d48b1db6d55bfda0736e53b17b7
Hong-Seungki <skgaboja@snu.ac.kr>
2020-09-09 10:34:03 +0900

    change source -> library

--- Example_Paper.R
+++ Example_Paper.R
@@ -2,11 +2,12 @@
 library(MASS)
 library(tidyverse)
 library(ClusterR)
-library(mclust)   
+library(mclust)
 library(cowplot)
-source('routines.R')
+library(ClusTorus)
+# source('routines.R')
 
-set.seed(20201) 
+set.seed(20201)
 # Prepare data 1 ------------------------------------------------------------
 
 # Example: five clusters
@@ -19,13 +20,13 @@ Sigma3 <- matrix(c(0.01,0,0,0.1),2,2)
 
 unidata <- cbind(2*runif(50, -0.5, 0.5), 0.5*runif(50, -0.5, 0.5))
 data.unif <- cbind(unidata[,1]+ 0, unidata[,2] + 1)
-data.diamond <- t(matrix(c(cos(-pi/4),-sin(-pi/4), 
+data.diamond <- t(matrix(c(cos(-pi/4),-sin(-pi/4),
                            sin(-pi/4),cos(-pi/4)),2,2) %*% t(unidata)) +cbind(rep(5, 50), rep(3, 50))
 
-Example1 <- rbind(mvrnorm(n=70, Mu1, Sigma1), 
-                  mvrnorm(n=50, Mu2, Sigma2), 
-                  mvrnorm(n=50, Mu3, Sigma3), 
-                  data.unif, 
+Example1 <- rbind(mvrnorm(n=70, Mu1, Sigma1),
+                  mvrnorm(n=50, Mu2, Sigma2),
+                  mvrnorm(n=50, Mu3, Sigma3),
+                  data.unif,
                   data.diamond)
 Example1 <- on.torus(Example1)
 label <- c(rep(1,70),rep(2,50),
@@ -37,13 +38,13 @@ colnames(dat1) <- c("phi","psi","label")
 
 unidata <- cbind(2*runif(50, -0.5, 0.5), 0.5*runif(50, -0.5, 0.5))
 data.unif <- cbind(unidata[,1]+ 0, unidata[,2] + 1)
-data.diamond <- t(matrix(c(cos(-pi/4),-sin(-pi/4), 
+data.diamond <- t(matrix(c(cos(-pi/4),-sin(-pi/4),
                            sin(-pi/4),cos(-pi/4)),2,2) %*% t(unidata)) +cbind(rep(5, 50), rep(3, 50))
 
-Example1 <- rbind(mvrnorm(n=70, Mu1, Sigma1), 
-                  mvrnorm(n=50, Mu2, Sigma2), 
-                  mvrnorm(n=50, Mu3, Sigma3), 
-                  data.unif, 
+Example1 <- rbind(mvrnorm(n=70, Mu1, Sigma1),
+                  mvrnorm(n=50, Mu2, Sigma2),
+                  mvrnorm(n=50, Mu3, Sigma3),
+                  data.unif,
                   data.diamond)
 Example1 <- on.torus(Example1)
 label <- c(rep(1,70),rep(2,50),
@@ -56,7 +57,7 @@ colnames(dat1.test) <- c("phi","psi","label")
 
 # Prepare data 2 ------------------------------------------------------------
 
-set.seed(2020) 
+set.seed(2020)
 # Example: ball and L shape
 Mu <- c(1,5)
 Sigma <- matrix(c(0.05,0,0,0.05),2,2)
@@ -65,9 +66,9 @@ unidata.unif <- cbind(runif(50, 0, 2*pi), runif(50, 0, 2*pi))
 data.unif1 <- cbind(1.25*runif(150, -1, 1)+ 3.25, 0.5*runif(150, -1, 1) + 1)
 data.unif2 <- cbind(0.5*runif(200, -1, 1)+ 5, 2*runif(200, -1, 1) + 2.5)
 
-Example1 <- rbind(mvrnorm(n=100, Mu, Sigma), 
-                  unidata.unif, 
-                  data.unif1, 
+Example1 <- rbind(mvrnorm(n=100, Mu, Sigma),
+                  unidata.unif,
+                  data.unif1,
                   data.unif2)
 Example1 <- Example1 + 2
 
@@ -81,9 +82,9 @@ unidata.unif <- cbind(runif(50, 0, 2*pi), runif(50, 0, 2*pi))
 data.unif1 <- cbind(1.25*runif(150, -1, 1)+ 3.25, 0.5*runif(150, -1, 1) + 1)
 data.unif2 <- cbind(0.5*runif(200, -1, 1)+ 5, 2*runif(200, -1, 1) + 2.5)
 
-Example1 <- rbind(mvrnorm(n=100, Mu, Sigma), 
-                  unidata.unif, 
-                  data.unif1, 
+Example1 <- rbind(mvrnorm(n=100, Mu, Sigma),
+                  unidata.unif,
+                  data.unif1,
                   data.unif2)
 Example1 <- Example1 + 2
 
@@ -99,14 +100,14 @@ colnames(dat2.test) <- c("phi","psi","label")
 
 
 # Figure ------------------------------------------------------------------
-g1 <- dat1 %>% ggplot(aes(x = phi, y = psi, color = label)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+g1 <- dat1 %>% ggplot(aes(x = phi, y = psi, color = label)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
-  ggtitle('Data set 1 with true labels') 
-g2 <- dat2 %>% ggplot(aes(x = phi, y = psi, color = label)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+  ggtitle('Data set 1 with true labels')
+g2 <- dat2 %>% ggplot(aes(x = phi, y = psi, color = label)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
-  ggtitle('Data set 2 with true labels') 
+  ggtitle('Data set 2 with true labels')
 
 plot_grid(g1,g2, label_size = 12)
 
@@ -116,17 +117,17 @@ ggsave("./examples/Toy_Data0.png", width = 8, height = 4)
 
 # Clustering by four methods (subfunction definition)------------------------------------------
 Example_paper_supp <- function(J, dat1, dat1.test){
-  
+
 data <- dat1[,1:2]
 data.test <- dat1.test
 
 data.test.label <- data.test[,3]
 data.test <- data.test[,1:2]
- 
+
 predicted.label <- matrix(NA, nrow = nrow(data), ncol =4)
 colnames(predicted.label) <- c("kmeans_naive","kmeans_amb","Proposal_ehat","Proposal_out")
 
-# Kmeans_naive 
+# Kmeans_naive
 kmeans.out<-KMeans_rcpp(data,clusters = J)
 predicted.label[,1] <- predict_KMeans(data.test, kmeans.out$centroids)
 
@@ -141,10 +142,10 @@ Jvec <- 3:35
 l <- list()
 
 # sample spliting; preparing data
-set.seed(2020) 
-n <- nrow(data) 
+set.seed(2020)
+n <- nrow(data)
 split.id <- rep(2,n)
-split.id[ sample(n,floor(n/2)) ] <- 1 
+split.id[ sample(n,floor(n/2)) ] <- 1
 
 for (j in Jvec){
   l[[j]] <- icp.torus.score(as.matrix(data), split.id = split.id,
@@ -153,7 +154,7 @@ for (j in Jvec){
                             param = list(J = j))
 }
 
-n2 <- l[[10]]$n2 
+n2 <- l[[10]]$n2
 alphavec <- 1:floor(n2/2) / n2
 N <- length(alphavec)
 out <- data.frame()
@@ -172,7 +173,7 @@ out[out.index,]
 
 Jhat <- out[out.index,2]
 alphahat <- out[out.index,1]
-icp.torus <- l[[Jhat]] 
+icp.torus <- l[[Jhat]]
 
 ia <- icp.torus.eval(icp.torus, level = alphahat, eval.point = grid.torus())
 b <- data.frame(ia$phi,ia$psi, ia$Chat_mix == 1, ia$Chat_max == 1, ia$Chat_e == 1)
@@ -180,11 +181,11 @@ colnames(b) <- c("phi","psi","C_mix","C_max","C_e")
 head(b)
 
 b<- b %>%  pivot_longer(3:5, names_to = "Type", values_to = "value")
-g0 <- ggplot() + geom_contour(aes(phi, psi, z = ifelse(value,1,0),linetype = Type, color =Type), data = b, size = 1,lineend = "round" ) + 
-  geom_point(mapping = aes(x,y), data = data.frame(x = data[,1],y =data[,2])) + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+g0 <- ggplot() + geom_contour(aes(phi, psi, z = ifelse(value,1,0),linetype = Type, color =Type), data = b, size = 1,lineend = "round" ) +
+  geom_point(mapping = aes(x,y), data = data.frame(x = data[,1],y =data[,2])) +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))
-g0 <- g0 + ggtitle(paste("ICP, vM2 mixture with J=",Jhat,", alpha=",alphahat)) 
+g0 <- g0 + ggtitle(paste("ICP, vM2 mixture with J=",Jhat,", alpha=",alphahat))
 g0
 
 #c <- cluster.assign.torus(data, icp.torus, level = alphahat)
@@ -196,49 +197,49 @@ predicted.label[,4] <- c$cluster.id.outlier
 
 aa <- rep(0,4)
 
-for (j in 1:4){ 
+for (j in 1:4){
   aa[j] <- adjustedRandIndex(predicted.label[,j],data.test.label)
   }
 
-g_e <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(c$cluster.id.by.ehat)) %>% 
-  ggplot(aes(phi,psi, color = membership)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+g_e <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(c$cluster.id.by.ehat)) %>%
+  ggplot(aes(phi,psi, color = membership)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   ggtitle(paste("Clustering by hat{e}, K=",c$ncluster))
- 
 
-g1 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(c$cluster.id.outlier)) %>% 
-  mutate(membership = ifelse(membership == max(c$cluster.id.outlier), "out", membership)) %>% 
-  ggplot(aes(phi,psi, color = membership)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+
+g1 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(c$cluster.id.outlier)) %>%
+  mutate(membership = ifelse(membership == max(c$cluster.id.outlier), "out", membership)) %>%
+  ggplot(aes(phi,psi, color = membership)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   ggtitle(paste("Clusters and outliers, K=",length(unique(c$cluster.id.outlier))))
 
 
-g_kmeans1 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(predicted.label[,1])) %>% 
-  ggplot(aes(phi,psi, color = membership)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+g_kmeans1 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(predicted.label[,1])) %>%
+  ggplot(aes(phi,psi, color = membership)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   ggtitle(paste("Kmeans , K=",J))
-  
 
-g_kmeans2 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(predicted.label[,2])) %>% 
-  ggplot(aes(phi,psi, color = membership)) + geom_point() + 
-  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+ 
+
+g_kmeans2 <- data.frame(phi = data.test[,1], psi = data.test[,2], membership = as.factor(predicted.label[,2])) %>%
+  ggplot(aes(phi,psi, color = membership)) + geom_point() +
+  scale_x_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   scale_y_continuous(breaks = c(0,1,2,3,4)*pi/2, labels = c("0","pi/2","pi","3pi/2","2pi"), limits = c(0,2*pi))+
   ggtitle(paste("Kmeans (ambient), K=",J))
 
 
-######################## Empirical coverage 
-# 
+######################## Empirical coverage
+#
 
 
 grid.test <- grid.torus()
 testing.n <- nrow(data)
 data.test <-as.matrix(data.test)
-# given a  C, for each testing item, see if it is included in C. 
+# given a  C, for each testing item, see if it is included in C.
 
-# aggregate C's 
+# aggregate C's
 
 alphavec <- 1:floor(n2/2) / n2
 alphavec <- alphavec[alphavec <= 0.4]
@@ -257,7 +258,7 @@ Inclusion.test <- matrix(0, nrow = testing.n, ncol = ncol(C))
 for (j in 1:testing.n){
   Inclusion.test[j,] <- C[which.min(rowSums( (t( t(grid.test) - data.test[j,]))^2) ),]
 }
-Out.dt <- rbind(Out.dt, 
+Out.dt <- rbind(Out.dt,
                 data.frame(alpha = alphavec, coverage = colMeans(Inclusion.test), Type = "C_mix"))
 
 C <- CC$Chat_max
@@ -267,7 +268,7 @@ for (j in 1:testing.n){
 }
 
 
-Out.dt <- rbind(Out.dt, 
+Out.dt <- rbind(Out.dt,
                 data.frame(alpha = alphavec, coverage = colMeans(Inclusion.test), Type = "C_max"))
 
 
@@ -282,12 +283,12 @@ Inclusion.test <- matrix(0, nrow = testing.n, ncol = ncol(C))
 for (j in 1:testing.n){
   Inclusion.test[j,] <- C[which.min(rowSums( (t( t(grid.test) - data.test[j,]))^2) ),]
 }
-Out.dt <- rbind(Out.dt, 
+Out.dt <- rbind(Out.dt,
                 data.frame(alpha = alphavec, coverage = colMeans(Inclusion.test), Type = "C_KDE"))
 
 
  head(Out.dt)
- g_cover <- Out.dt %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() + 
+ g_cover <- Out.dt %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() +
      geom_point() + geom_abline(slope = 1, intercept = 0) + geom_vline(xintercept =  1- alphahat,linetype = "dotted" )
 
 
@@ -305,7 +306,7 @@ Out.dt <- rbind(Out.dt,
 
 
 
-list(choice = out[out.index,], labels = predicted.label, Rand = aa, clustering.result = c, 
+list(choice = out[out.index,], labels = predicted.label, Rand = aa, clustering.result = c,
      gg = g0,ge = g_e, gout = g1, g_k1 = g_kmeans1, g_k2 = g_kmeans2, coverage = Out.dt, g_cover  = g_cover )
 }
 
@@ -323,12 +324,12 @@ dat2.results$Rand
 plot_grid(dat1.results$gg ,
           dat2.results$gg , label_size = 12)
 ggsave("./examples/Toy_Data1.png", width = 8, height = 4)
-          
-plot_grid( 
+
+plot_grid(
 dat1.results$g_k1 ,
-          dat2.results$g_k1 , 
+          dat2.results$g_k1 ,
 dat1.results$g_k2 ,
-          dat2.results$g_k2 , 
+          dat2.results$g_k2 ,
 dat1.results$ge ,
           dat2.results$ge ,
 dat1.results$gout ,
@@ -339,10 +340,10 @@ ggsave("./examples/Toy_Data2.png", width = 12, height = 16)
 dat1.results$Rand
 dat2.results$Rand
 
-l1<- dat1.results$coverage %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() + 
-  geom_point() + geom_abline(slope = 1, intercept = 0)  
-l2 <-dat2.results$coverage %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() + 
-  geom_point() + geom_abline(slope = 1, intercept = 0)  
+l1<- dat1.results$coverage %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() +
+  geom_point() + geom_abline(slope = 1, intercept = 0)
+l2 <-dat2.results$coverage %>% ggplot(aes(1-alpha, coverage,color = Type, group = Type))  + geom_line() +
+  geom_point() + geom_abline(slope = 1, intercept = 0)
 
 
 plot_grid(l1 ,
