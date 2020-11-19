11b86c7abbda14211a2c2c2898ea0c031471fe0a
Hong-Seungki <skgaboja@snu.ac.kr>
2020-11-19 23:48:39 +0900

    update for vectorization and code efficiency

--- R/tor.minus.R
+++ R/tor.minus.R
@@ -21,11 +21,17 @@
 tor.minus <- function(data, mu){
   # data is n x d matrix of toroidal values, mu is a d-vector.
   # returns toroidal subtraction
+  if(is.vector(data)){ data <- t(as.matrix(data)) }
   # cbind(
-  #   ang.minus(data[,1], mu[1]),
-  #   ang.minus(data[,2], mu[2]))
-  
+  #  ang.minus(data[,1], mu[1]),
+  #  ang.minus(data[,2], mu[2]))
+
   # for higher dimension -----------
-  t(apply(data, 1, ang.minus, Mu))
-  
+  # t(apply(data, 1, ang.minus, mu))
+  tor <- ang.minus(data[,1], mu[1])
+  d <- length(mu)
+  for (i in 2:d){
+    tor <- cbind(tor, ang.minus(data[,i], mu[i]))
+  }
+  tor
 }
