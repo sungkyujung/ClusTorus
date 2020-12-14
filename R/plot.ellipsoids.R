f2cdc157051e17bd158067c4590df83352bedb5d
Hong-Seungki <skgaboja@snu.ac.kr>
2020-12-14 14:54:17 +0900

    new function: plot ellipses on 2-dimensional toroidal space

---
+++ R/plot.ellipsoids.R
@@ -0,0 +1,67 @@
+#' Plot Ellipsoids on 2-dimensional Toroidal Space
+#'
+#' \code{plot.ellipsoids} generates a 2-dimensional plot for the given ellipsoids,
+#'   with prespecified coordinates.
+#'
+#' @param list which is consisting of mean of each angular
+#'   coordinate, inverse of each covariance matrix, and constant term
+#' @param t a numeric value which determines the size of ellipses.
+#' @param coord a 2-vector for prespecifing the coordinates.
+#'   Default value is c(1, 2).
+#' @return a ggplot2-based 2-dimensional plot with ellipses
+#' @export
+#' @seealso \code{\link[ClusTorus]{icp.torus.score}}, \code{\link[ClusTorus]{cluster.assign.torus}}
+#' @examples
+#' \dontrun{
+#' parammat <- matrix(c(0.4, 0.3, 0.3,
+#'                      20, 25, 25,
+#'                      30, 25, 20,
+#'                      1, 2, 3,
+#'                      1, 2, 3,
+#'                      0, 2, 4), nrow = 6, byrow =TRUE)
+#'
+#' ellipse.param <- norm.appr.param(parammat)
+#'
+#' coord <- c(1, 3)
+#' t <- 0.5
+#'
+#' plot.ellipsoids(ellipse.param, t, coord)
+#' }
+
+plot.ellipsoids <- function(ellipse.param, t, coord = c(1, 2)){
+  if(length(coord) != 2){stop("Invalid coordinates. coord must be a 2-dimensional numeric vector.")}
+
+  g2 <- ggplot2::ggplot()
+  J <- length(ellipse.param$c)
+  theta <- seq(0, 2 * pi, length.out = 999)
+  Z <- cbind(cos(theta), sin(theta))
+
+  shift <- matrix(0, ncol = 2, nrow = 9)
+  shift[, 1] <- c(0, 2 * pi, -2 * pi)
+  shift[, 2] <- rep(c(0, 2 * pi, -2 * pi), each = 3)
+
+  for (j in 1:J){
+    mu <- ellipse.param$mu[j, coord]
+    Sinv <- ellipse.param$Sigmainv[[j]][coord, coord]
+    c.minus.t <- ellipse.param$c[j] - t
+
+    if (c.minus.t <= 0) {next}
+
+    M <- eigen(Sinv/c.minus.t)
+    Mmhalf <- M$vectors %*% diag( sqrt(1/M$values) ) %*% t(M$vectors)
+    R <- Mmhalf %*% t(Z)
+    for( shift.id in 1:9){
+      RR <- R + mu + shift[shift.id,]
+      g2 <- g2 + ggplot2::geom_polygon(aes(x = angle1, y = angle2),color = "blue",alpha = 0.1,
+                                      data = data.frame(angle1 = RR[1,],angle2 = RR[2,], value = 1))
+    }
+  }
+
+  g <- g2 + ggplot2::scale_x_continuous(breaks = c(0,1,2)*pi,
+                       labels = c("0","pi", "2pi")) +
+    ggplot2::scale_y_continuous(breaks = c(0,1,2)*pi,
+                       labels = c("0","pi","2pi")) +
+    ggplot2::coord_cartesian(xlim = c(0, 2*pi), ylim = c(0, 2*pi), expand = FALSE)
+
+  g
+}
