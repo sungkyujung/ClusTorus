c1a9ce250e98a5fd52d072a1a8c688814ecdc85c
Hong-Seungki <skgaboja@snu.ac.kr>
2020-12-14 14:55:18 +0900

    update for invalid input

--- R/Ainv.R
+++ R/Ainv.R
@@ -16,7 +16,9 @@
 #' \dontrun{
 #' Ainv(0.7, kmax = 3000)
 #' }
-Ainv <- function (x,kmax = 2000){
+Ainv <- function (x, kmax = 2000){
+  if (!is.numeric(x) || length(x) > 1) {stop("Invalid value to x : x must be in the unit interval.")}
+  if (!is.numeric(kmax) || length(kmax) > 1) {stop("Invalid value to kmax : kmax must be a numeric scalar.")}
   k <- ifelse(0 <= x & x < 0.53, 2 * x + x^3 + (5 * x^5)/6,
               ifelse(x < 0.85, -0.4 + 1.39 * x + 0.43/(1 - x),
                      ifelse(x < 1,1/(x^3 - 4 * x^2 + 3 * x), kmax)))
